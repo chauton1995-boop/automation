b9de18bcb51413fb0697676fb0e0ffa915f11db7
Tuan <chauton1995@gmail.com>
2025-09-09 21:04:14 +0700

    add new code

--- automation_framework/tests/test_login.py
+++ automation_framework/tests/test_login.py
@@ -2,21 +2,18 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+from base.base_test import BaseTest 
 
-def test_login():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+class TestOrange(BaseTest):
+    USERNAME = (By.NAME, "username")
+    PASSWORD = (By.NAME, "password")
+    LOGIN_BUTTON = (By.XPATH, "//button[@type='submit']")
+    def test_login(self):
+        username = self.driver.find_element(*self.USERNAME)
+        password = self.driver.find_element(*self.PASSWORD)
+        login_button = self.driver.find_element(*self.LOGIN_BUTTON)
+        username.send_keys("Admin")
+        password.send_keys("admin123")
+        login_button.click()
+        sleep(5)
 
-    sleep(2)  # đợi trang load
-
-    username = driver.find_element(By.NAME, "username")
-    password = driver.find_element(By.NAME, "password")
-    login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-
-    username.send_keys("Admin")
-    password.send_keys("admin123")
-    login_button.click()
-
-    sleep(5)
-    driver.quit()
